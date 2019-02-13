8bd2e4a0a91f22857cee17bab7e5b3532f5ab2fb
huynh <caubequay00@gmail.com>
2019-02-13 13:56:04 +0700

    new

---
+++ node-lmdb/build/Release/.deps/Release/obj.target/node-lmdb/dependencies/lmdb/libraries/liblmdb/mdb.o.d
@@ -0,0 +1,8 @@
+cmd_Release/obj.target/node-lmdb/dependencies/lmdb/libraries/liblmdb/mdb.o := cc '-DNODE_GYP_MODULE_NAME=node-lmdb' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/jeffrey/.node-gyp/10.15.1/include/node -I/home/jeffrey/.node-gyp/10.15.1/src -I/home/jeffrey/.node-gyp/10.15.1/deps/openssl/config -I/home/jeffrey/.node-gyp/10.15.1/deps/openssl/openssl/include -I/home/jeffrey/.node-gyp/10.15.1/deps/uv/include -I/home/jeffrey/.node-gyp/10.15.1/deps/zlib -I/home/jeffrey/.node-gyp/10.15.1/deps/v8/include -I../../nan -I../dependencies/lmdb/libraries/liblmdb  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -fvisibility=hidden -O3 -fno-omit-frame-pointer  -MMD -MF ./Release/.deps/Release/obj.target/node-lmdb/dependencies/lmdb/libraries/liblmdb/mdb.o.d.raw   -c -o Release/obj.target/node-lmdb/dependencies/lmdb/libraries/liblmdb/mdb.o ../dependencies/lmdb/libraries/liblmdb/mdb.c
+Release/obj.target/node-lmdb/dependencies/lmdb/libraries/liblmdb/mdb.o: \
+ ../dependencies/lmdb/libraries/liblmdb/mdb.c \
+ ../dependencies/lmdb/libraries/liblmdb/lmdb.h \
+ ../dependencies/lmdb/libraries/liblmdb/midl.h
+../dependencies/lmdb/libraries/liblmdb/mdb.c:
+../dependencies/lmdb/libraries/liblmdb/lmdb.h:
+../dependencies/lmdb/libraries/liblmdb/midl.h:
---
+++ node-lmdb/build/Release/.deps/Release/obj.target/node-lmdb/src/dbi.o.d
@@ -0,0 +1,60 @@
+cmd_Release/obj.target/node-lmdb/src/dbi.o := g++ '-DNODE_GYP_MODULE_NAME=node-lmdb' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/home/jeffrey/.node-gyp/10.15.1/include/node -I/home/jeffrey/.node-gyp/10.15.1/src -I/home/jeffrey/.node-gyp/10.15.1/deps/openssl/config -I/home/jeffrey/.node-gyp/10.15.1/deps/openssl/openssl/include -I/home/jeffrey/.node-gyp/10.15.1/deps/uv/include -I/home/jeffrey/.node-gyp/10.15.1/deps/zlib -I/home/jeffrey/.node-gyp/10.15.1/deps/v8/include -I../../nan -I../dependencies/lmdb/libraries/liblmdb  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -fPIC -fvisibility=hidden -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++1y -fPIC -fvisibility=hidden -fvisibility-inlines-hidden -std=c++0x -MMD -MF ./Release/.deps/Release/obj.target/node-lmdb/src/dbi.o.d.raw   -c -o Release/obj.target/node-lmdb/src/dbi.o ../src/dbi.cpp
+Release/obj.target/node-lmdb/src/dbi.o: ../src/dbi.cpp ../src/node-lmdb.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/v8.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/v8-version.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/v8config.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/v8.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/v8-platform.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node_version.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node_buffer.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node.h ../../nan/nan.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node_version.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv/errno.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv/version.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv/unix.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv/threadpool.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/uv/linux.h \
+ /home/jeffrey/.node-gyp/10.15.1/include/node/node_object_wrap.h \
+ ../../nan/nan_callbacks.h ../../nan/nan_callbacks_12_inl.h \
+ ../../nan/nan_maybe_43_inl.h ../../nan/nan_converters.h \
+ ../../nan/nan_converters_43_inl.h ../../nan/nan_new.h \
+ ../../nan/nan_implementation_12_inl.h ../../nan/nan_persistent_12_inl.h \
+ ../../nan/nan_weak.h ../../nan/nan_object_wrap.h ../../nan/nan_private.h \
+ ../../nan/nan_typedarray_contents.h ../../nan/nan_json.h \
+ ../dependencies/lmdb/libraries/liblmdb/lmdb.h
+../src/dbi.cpp:
+../src/node-lmdb.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/v8.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/v8-version.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/v8config.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/v8.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/v8-platform.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node_version.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node_buffer.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node.h:
+../../nan/nan.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node_version.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv/errno.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv/version.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv/unix.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv/threadpool.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/uv/linux.h:
+/home/jeffrey/.node-gyp/10.15.1/include/node/node_object_wrap.h:
+../../nan/nan_callbacks.h:
+../../nan/nan_callbacks_12_inl.h:
+../../nan/nan_maybe_43_inl.h:
+../../nan/nan_converters.h:
+../../nan/nan_converters_43_inl.h:
+../../nan/nan_new.h:
+../../nan/nan_implementation_12_inl.h:
+../../nan/nan_persistent_12_inl.h:
+../../nan/nan_weak.h:
+../../nan/nan_object_wrap.h:
+../../nan/nan_private.h:
+../../nan/nan_typedarray_contents.h:
+../../nan/nan_json.h:
+../dependencies/lmdb/libraries/liblmdb/lmdb.h:
